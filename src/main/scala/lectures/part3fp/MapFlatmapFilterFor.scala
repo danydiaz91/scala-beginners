19a0f09f2d5668e13d57c4a8b707b7e8f32b5e4b
Dany Diaz <dany.diaz91@hotmail.com>
2021-12-08 16:43:57 -0500

    feat: map, flatMap, filter and for-comprehensions

---
+++ src/main/scala/lectures/part3fp/MapFlatmapFilterFor.scala
@@ -0,0 +1,44 @@
+package lectures.part3fp
+
+object MapFlatmapFilterFor extends App {
+
+  val list = List(1, 2, 3)
+  println(list.head)
+  println(list.tail)
+
+  // map
+  println(list.map(_ + 1))
+  println(list.map(_ + " is a number"))
+
+  // filter
+  println(list.filter(_ % 2 == 0))
+
+  // flatMap
+  val toPair = (x: Int) => List(x, x + 1)
+  println(list.flatMap(toPair))
+
+  // print all combinations between two lists
+  val numbers = List(1, 2, 3, 4)
+  val chars = List('a', 'b', 'c', 'd')
+
+  val combinations = numbers.flatMap(n => chars.map(c => "" + c + n))
+  println(combinations)
+
+  // foreach
+  list.foreach(println)
+
+  // for-comprehensions
+  val forCombinations = for {
+    n <- numbers if n % 2 == 0
+    c <- chars
+  } yield "" + c + n
+
+  println(forCombinations)
+
+  // syntax overload
+  list.map { x =>
+    x * 2
+  }
+
+
+}
